7811f2955347bffdaab51dc8d84ce797a73f6ef3
Gabriel Baril <gabriel-baril2010@hotmail.com>
2024-12-25 00:39:51 -0500

    Added sharpmake file and refactored file structure for easier integration

---
+++ inih.sharpmake.cs
@@ -0,0 +1,26 @@
+using System.IO; // For Path.Combine
+using Sharpmake; // Contains the entire Sharpmake object library.
+
+[Generate]
+public class INIHProject : BaseCppProject
+{
+    public INIHProject()
+    {
+        Name = "inih";
+        SourceRootPath = @"[project.SharpmakeCsPath]\src";
+        AddTargets(TargetUtil.DefaultTarget);
+    }
+
+    [Configure]
+    public new void ConfigureAll(Project.Configuration conf, Target target)
+    {
+        base.ConfigureAll(conf, target);
+
+        conf.SolutionFolder = Constants.EXTERNAL_VS_CATEGORY;
+
+        conf.Output = Project.Configuration.OutputType.Lib;
+        conf.TargetPath = @"[project.SharpmakeCsPath]\Out\Bin\[target.Platform]-[target.Optimization]";
+        conf.IntermediatePath = @"[project.SharpmakeCsPath]\Out\Intermediate\[target.Platform]-[target.Optimization]";
+        conf.IncludePaths.Add(@"[project.SharpmakeCsPath]\src");
+    }
+}
